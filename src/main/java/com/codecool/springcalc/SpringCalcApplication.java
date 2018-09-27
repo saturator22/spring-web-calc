c3891a69a22b54f47615b519bdebbb01fa49e2ce
saturator22 <f.szczeski@gmail.com>
2018-09-27 15:49:43 +0200

    Add main App

---
+++ src/main/java/com/codecool/springcalc/SpringCalcApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.springcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCalcApplication.class, args);
+	}
+}
