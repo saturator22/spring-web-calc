d78607389fa8786dd49f8c02cb05c5210ca92787
saturator22 <f.szczeski@gmail.com>
2018-09-27 15:54:09 +0200

    Add Calculatable interface

---
+++ src/main/java/com/codecool/springcalc/Services/Calculatable.java
@@ -0,0 +1,8 @@
+package com.codecool.springcalc.Services;
+
+import com.codecool.springcalc.Model.CalculatorModel;
+
+public interface Calculatable {
+
+    double calculate(CalculatorModel calculatorModel);
+}
